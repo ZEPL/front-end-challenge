a257e89a6d17d66ccc132ffd2b7b6ac6dfcf3ae9
HMarc <marc.hurabielle@gmail.com>
2016-12-03 12:09:39 +0100

    Program skeletons

---
+++ serialize-state/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  render () {
+    return (
+      <div>TEST</div>
+    )
+  }
+}
