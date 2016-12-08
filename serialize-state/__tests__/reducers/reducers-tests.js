5c7fa45281d3d6e6d03379f99bbe7a9063a3ac9b
ess_ess <sravans2011@gmail.com>
2016-12-08 10:00:34 +0530

    Add test for reducers

--- serialize-state/__tests__/reducers/reducers-tests.js
+++ serialize-state/__tests__/reducers/reducers-tests.js
@@ -8,4 +8,57 @@ describe('reducers', () => {
       reducer(undefined, {})
     ).toEqual(initialState);
   });
-})
+
+  it('should change the user name', () => {
+    expect(
+      reducer(initialState, {
+        type: MUTATE_FORM,
+        state: {
+          userName: 'Julian'
+        }
+      })
+    ).toEqual({...initialState, ...{
+      userName: 'Julian'
+    }});
+  });
+
+  it('should change the selected comic', () => {
+    expect(
+      reducer(initialState, {
+        type: MUTATE_FORM,
+        state: {
+          comic: DC
+        }
+      })
+    ).toEqual({...initialState, ...{
+      comic: DC
+    }});
+  });
+
+  it('should change the selected super hero', () => {
+    expect(
+      reducer(initialState, {
+        type: MUTATE_FORM,
+        state: {
+          superHeroes: [BATMAN]
+        }
+      })
+    ).toEqual({...initialState, ...{
+      superHeroes: [BATMAN]
+    }});
+  });
+
+  it('should set the state', () => {
+    let newState = {
+      userName: 'Julian',
+      comic: DC,
+      superHeroes: [BATMAN]
+    };
+    expect(
+      reducer(undefined, {
+        type: SET_STATE,
+        state: newState
+      })
+    ).toEqual(newState);
+  });
+});
